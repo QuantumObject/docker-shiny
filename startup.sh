970e8de9877c0137bb0e83a09f9aa9877c35961e
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-01 20:12:44 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-
 set -e
 
+#in case Volume are empty
+if [ "$(ls -A /srv/shiny-server)" ]; then
+    echo "shiny-server folder with data"    
+else
+    cp -Rp /var/backup/shiny-server/* /srv/shiny-server/ 
+    chown -R shiny:shiny /srv/shiny-server
+fi
+
+
 if [ -f /etc/configured ]; then
-        chown -R shiny:shiny /srv/shiny-server
-        chown -R shiny:shiny /var/lib/shiny-server
         echo 'already configured'
 else
       #code that need to run only one time ....
